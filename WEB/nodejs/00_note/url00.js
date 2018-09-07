50aedc283dc97e415b1cf862afb7df93397bd4f4
jyjung621 <jyjung621@gmail.com>
2018-09-07 15:10:13 +0900

    nodejs

---
+++ WEB/nodejs/00_note/url00.js
@@ -0,0 +1,12 @@
+/*
+ * URL의 형식
+ * 
+ * http://opentutorial.org:3000/main?id=HTML&page=12
+ * 
+ * http -> protocol
+ * opentutorial.org -> host(domain)
+ * 3000 -> port
+ * main -> path
+ * id=HTML&page=12 -> query string (시작은 ?로 표시)
+ * 
+ */
